e6b9cfd83362295cead2b0f7c525f2bb0ffeecbb
vorpus <li3zhang@gmail.com>
2016-11-25 09:57:23 -0500

    fix lag and create resizable view

--- js/bundle.js
+++ js/bundle.js
@@ -223,26 +223,41 @@
 	    this.game = game;
 	    this.$view = $view;
 	    this.board = new Board();
+	    this.makeGrid();
 	    this.render();
 	    $('body').on("keydown", (ev) => {
 	      this.handleKeyEvent(ev);
 	    });
+	    $('.snake').on("orientationchange", (ev) => {
+	      console.log('swiped');
+	      this.handleSwipeEvent(ev);
+	    });
 	
-	    // window.setTimeout(() => {
-	      window.setInterval(() => {
-	        this.step();
-	        this.render();
-	        if (this.board.snakeBounds() || this.board.snakeSuicide()) {
-	          this.board.deadSnake();
-	        }
+	    this.squareGrid();
 	
-	      }, 50);
+	
+	    window.setInterval(() => {
+	      this.step();
+	      this.render();
+	      if (this.board.snakeBounds() || this.board.snakeSuicide()) {
+	        this.board.deadSnake();
+	      }
+	
+	    }, 60);
 	
 	  }
 	
-	  render() {
-	    let grid = this.board.grid;
-	    this.$view.empty();
+	  squareGrid() {
+	    var mapElement = $("li");
+	    mapElement.css('height', mapElement.css('width'));
+	
+	    window.addEventListener("resize", function(e) {
+	      var mapElement = $("li");
+	      mapElement.css('height', mapElement.css('width'));
+	    });
+	  }
+	
+	  makeGrid() {
 	    this.board.grid.forEach( (el, i) => {
 	      let $gridRow = $('<ul class="group"></ul>');
 	      el.forEach( (_,j) => {
@@ -250,6 +265,11 @@
 	      });
 	      this.$view.append($gridRow);
 	    });
+	  }
+	
+	  render() {
+	    let grid = this.board.grid;
+	    $('li').css('background','lightblue');
 	    // this.$view.each(() => {
 	    //   console.log(this);
 	    // });
@@ -287,6 +307,27 @@
 	      default:
 	        console.log('invalid key');
 	    }
+	    // console.log(this.board.snake.direction);
+	  }
+	
+	  handleSwipeEvent(ev) {
+	    console.log(ev);
+	    // switch(String.fromCharCode(ev.which)) {
+	    //   case "&":
+	    //     this.board.snake.turn("N");
+	    //     break;
+	    //   case "'":
+	    //     this.board.snake.turn("E");
+	    //     break;
+	    //   case "%":
+	    //     this.board.snake.turn("W");
+	    //     break;
+	    //   case "(":
+	    //     this.board.snake.turn("S");
+	    //     break;
+	    //   default:
+	    //     console.log('invalid key');
+	    // }
 	    // console.log(this.board.snake.direction);
 	  }
 	
---
+++ readme.md
@@ -0,0 +1,13 @@
+[https://vorpus.github.io/snake_js/](https://vorpus.github.io/snake_js/)
+
+# Snake todo:
+## features
+- Points
+- High scores
+- Variable difficulty
+- Breather between games
+
+
+## efficiency
+- Combine checking if snake has collided with itself/apple
+- Render to only repaint cells that were previously colored.
